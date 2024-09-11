bb4d4602a4f2a71be6c84673f6936f8728af0d8f
Letícia Souza <138714479+LetiSouza18@users.noreply.github.com>
2024-09-11 12:31:01 -0300

    Add files via upload

---
+++ codigo_para_thonny - projeto 2.py
@@ -0,0 +1,194 @@
+texto_port = """
+NOSSO POVO
+Aqui começa a história do tempo dos antigos 
+O início da história 
+01 Antes do mundo existir,
+02 Ngutapa já existia.
+03 Ele não teve pai nem mãe.
+04 Mapana,
+05 a mulher de Ngutapa,
+06 se criou junto com ele.
+07 No mesmo lugar viviam também Baia e sua mulher.
+08 Baia era parente de Ngutapa.
+09 No lugar onde esses quatro se criaram é onde ficava a montanha Taiwegüne,
+10 E no igarapé Tonetü.
+11 Naquele tempo,
+12 a terra ainda estava se formando.
+13 O mato era baixinho e o rio ainda tinha pouca água,
+14 lá eles viviam.
+15 Passaram-se muitos anos.
+16 Ngutapa e Mapana nunca habitaram juntos.
+17 Nunca tiveram filhos.
+18 Um dia,
+19 quando o mato já estava crescido,
+20 Ngutapa foi caçar com Mapana.
+21 No caminho eles começaram a brigar.
+22 Ngutapa agarrou sua mulher e lhe deu uma surra.
+23 Depois disso,
+24 amarrou Mapana
+25 num pau,
+26 de braços e pernas abertos.
+27 Deixou ela ali e seguiu para caçar no mato.
+28 Vieram as cabas e as formigas e morderam a sua periquita.
+29 Ela sofreu muito.
+30 Então apareceu o pássaro cancã e se sentou no alto do pau onde ela estava amarrada.
+31 Mapana disse para o cancã:
+32 Vovó,
+33 pode me desamarrar?
+34 O pássaro gritou:
+35 Co, co, co, cou!
+36 Vovó,
+37 venha me desamarrar. Aquele desgraçado me prendeu aqui para me matar
+38 ela falou de novo.
+39 O cancã se transformou em gente e,
+40 chegando mais perto,
+41 perguntou:
+42 O que lhe aconteceu,
+43 minha neta?
+44 Se você quiser se vingar de Ngutapa,
+45 está aqui a caba.
+46 Ela pegou a caba e guardou.
+47 A casa de caba era muito grande,
+48 mas parecia pequena.
+49 O cancã falou ainda:
+50 Você não pode ficar aí.
+51 Tem que esperar o seu marido num lugar onde ele não possa ver.
+52 Depois disso,
+53 o cancã se transformou em pássaro e foi embora.
+54 Demorou um pouco e Ngutapa voltou da caçada.
+55 Vinha tocando flauta e pulava numa perna e noutra,
+56 cantando:
+57 Por onde anda Mapana?!
+58 As cabas e as formigas morderam a periquita dela!
+59 Tcheruru tcheruru-u-u-u ... Tcheruru tcheruru-u-u-u ... - assim dizia.
+60 Mapana estava escondida num tronco de árvore,
+61 esperando Ngutapa passar.
+62 Escutou esse canto e se preparou.
+63 Quando ele chegou,
+64 ela jogou a casa de caba em cima dele e acertou-lhe os dois joelhos.
+65 Ngutapa caiu e não se levantou mais.
+66 Mapana deixou ele ali mesmo e foi embora.
+67 Ngutapa foi se arrastando até em casa.
+68 Desde que as cabas ferraram,
+69 seus joelhos começaram a inchar.
+""" 
+
+texto_ticuna = """
+TORÜ DUǕ'ǕGÜ
+Nori arü ügü ga ore ga nucüma'ǖ ga torü ga na nhunhaacü yi'i'ǖ.
+Norü ügü tchiga 
+01 Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,
+02 rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ.
+03 Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa.
+04 I nori'ǖtchicü,
+05 ingeguma,
+06 rü norü tchiura ta ngemana yaǖ,
+07 rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ.
+08 Erü nüma ga Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.
+09 Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau,
+10 ga namamaã rü na yaemare.
+11 Ngeguma yema namamare i ya'ĩ,
+12 rü tauguma i buǖã.
+13 Ga ngĩma ga Mapana,
+14 rü yemaca ngitchi naai ga nüma ga Ngutapa.
+15 Inacü i ngeguma i ngema.
+16 I Taiwegüne i nori naãene i üǖwacü rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua natü i Tonetü.
+17 Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ.
+18 Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ,
+19 rü marü nhurema ya taunecǖ na yemagü rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa rü yeguma rü na tauma ga to,
+20 ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ.
+21 Natürü ga guma Baia rü nü'ǖ tayema ga bue rü naaãcü nǖetcha nüma ga guma.
+22 Ngutapa rü tauguma naãcü rü mu'utchicüma ya tauenecü na ngeacü.
+23 Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa,
+24 yerü yeguma rü marü nayaanegü ga naĩnecü rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama,
+25 rü yeguma tü'ǖ ya naĩ'ü rü nainewa.
+26 tü'ǖ ni'ĩ nai rü ngiparawa rüta tü'ǖ ni'ĩ naĩgü rü ngĩtchacü'ǖwa rüta.
+27 Rü yemawena rü nüma ga.
+28 Ngutapa rü na üpetü ga na ya feneǖ.
+29 Yerü yegumarü marü auriǖtchima nguǖ tinge rü maẽ rü tü'ǖ nawi rü ẽne rü tü'ü na ngo ga tümaãrü ngaǖwa.
+30 Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü rü nhãtagürü:
+31 Pa noẽ,
+32 pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu nhãtagürü co-co-co-cou nhatagüǖ?
+33 tauchina meǖ ega tchoǖ icuiwẽgügu rü tchoǖ ni'ĩ ma i ucaǖǖtchi i Ngutapa nhãtagürü:
+34 Rü yeguma ga noẽ ga Co, co, co, cou rü ĩrü duǖ!
+35 pa noẽ,
+36 Rü yeguma rü tü'ǖ ĩ ya wẽgü.
+37 Rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü
+38 rü yeguma rü inayau ga maẽ ga Mapana.
+39 Rü yeguma ga noẽ i coou ta ngica itarü ĩ,
+40 rü ngiǖ ta ngaǖ rü cu tücü,
+41 pa tchauta'ã nhãtagürü:
+42 Natürü ga guma maẽ rü naega,
+43 rü ngerata ni'ĩ ga naẽga,
+44 natütü guma maẽ,
+45 rü na taütchirema.
+46 Rü natürü yiema i daugu rü na i'ra.
+47 Rü yeguma i nayau ga maẽ,
+48 rü ngigürügü taũǖ tama ni'ĩ nua.
+49 cuǖna ngẽẽǖ ngigürügü:
+50 Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü.
+51 Rü yeguma ga nüma ga Ngutapa rü marü na taegutchaǖ.
+52 Rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü,
+53 rü yeguma ga ngima ga Mapana rü marü iya nguẽẽ ga namacüwawa ga naĩpünewa rü nüma ga Ngutapa rü ni'ĩ woweruetchigü.
+54 Rü ni'ĩ wü'i paratchigü.
+55 rü ni'ĩ fenagütchigü,
+56 rü nhãnagürü:
+57 nhũmarütchi nhũnhã i Mapana?!
+58 i maẽ ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü!
+59 Tcherutcheru-u-u-u ... tcherurutcheruru-u-u-u ... - nhanagürü.
+60 Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ,
+61 Rü wüiparagu ni'ĩ ga nüma rü yeguma marü na ngupetüatchigu.
+62 rü nügü i'ya caiãtchigu rü yeguma naweama nenanhãga guma maẽ.
+63 Rü pe ga naã pü'ügu rü norü naiãpüüguta.
+64 Rü ngǖ ga ini'ĩ ni'ĩngu,
+65 rü marü tama yema i natchi rü tüma ga norü na'ma ga Mapana,
+66 rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa.
+67 Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga naãpüǖ rü marü manarütchaapüǖ,
+68 rü yemaãcü tama ini'ĩu rü ini waatchigü,
+69 Rü nhumata inangu ga ĩpatawa ga nayaanegü.
+"""
+
+
+texto_instrucoes = """
+################################################################################
+# 1- ESTE PROGRAMA NÃO É UM TRADUTOR PROPRIAMENTE DITO,                        #
+# TRATA-SE DE UM CORRELACIONADOR ENTRE FRASES EM PORTUGUÊS                     #
+# E NA LINGUAGEM TICUNA.                                                       #
+#                                                                              #
+# Adicionar informações                                                        #
+#                                                                              #
+################################################################################
+"""
+
+print("Seja bem-vindo(a) ao nosso correlacionador de frases português/ticuna.\n")
+lista = list()
+dicionario = {}
+
+for i in range(0, len(texto_port), 2):
+    if texto_port[i].isnumeric():
+        #Achar a frase em português correspondente a cada número:
+        num = texto_port[i] + texto_port[i + 1]
+        pos_num_port = int(texto_port.find(num))
+        pos_prox_num_port = int(texto_port.find(str(int(num) + 1)))
+        tam_frase_port = pos_prox_num_port - pos_num_port
+        frase_port =  texto_port[pos_num_port + 2 : pos_num_port + tam_frase_port - 2] 
+        
+        #Achar a frase em ticuna correpondente a cada número:
+        pos_num_ticuna = int(texto_ticuna.find(num))
+        pos_prox_num_ticuna = int(texto_ticuna.find(str(int(num) + 1)))
+        tam_frase_ticuna = pos_prox_num_ticuna - pos_num_ticuna
+        frase_ticuna =  texto_ticuna[pos_num_ticuna + 2 : pos_num_ticuna + tam_frase_ticuna - 2] 
+        
+        #Armazenar em um dicionário o id, port, tic
+        dicionario['port'] = frase_port
+        dicionario['ticuna'] = frase_ticuna
+        dicionario['id'] = num
+        lista.append(dicionario)
+        dicionario = {}
+        
+print(lista)
+
+
+
+
+    
